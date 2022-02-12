b6db3fbb3bd10eec70f695295078c80b66173bea
marco <marcopablodemaio@gmail.com>
2022-02-12 19:52:11 -0300

    WIP deploy

--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,61 +1,61 @@
-import React from 'react';
-import { useRouter } from 'next/router'
-import axios from 'axios';
+// import React from 'react';
+// import { useRouter } from 'next/router'
+// import axios from 'axios';
 
 
 
-//------getStaticPaths------//
-export async function getStaticPaths() {
+// //------getStaticPaths------//
+// export async function getStaticPaths() {
 
-  const URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000';
+//   const URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000';
 
-  const resp_products = await axios.get(`${URL}/products`);
+//   const resp_products = await axios.get(`${URL}/products`);
 
-  const paths = resp_products.data.map((product) =>
-   {
-      return {params:{
-        id: toString(product.id_product)
-      }}
-    }
-  );
+//   const paths = resp_products.data.map((product) =>
+//    {
+//       return {params:{
+//         id: toString(product.id_product)
+//       }}
+//     }
+//   );
 
-  return {
-    paths,
-    fallback: true
-  };
-}
-
-
-//------getStaticProps------//
-export async function getStaticProps({params}){
-
-  const URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000';
-
-  const resp_product = await axios.get(`${URL}/products/${params.id}`);
-
-  return {
-      props: {
-          product: resp_product.data,
-      }
-  }
-};
+//   return {
+//     paths,
+//     fallback: true
+//   };
+// }
 
 
+// //------getStaticProps------//
+// export async function getStaticProps({params}){
 
-//------COMPONENTE------//
-export default function ProductPage({product}) {
-  // const router = useRouter();
-  // const {id} = router.query;
+//   const URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8000';
 
-  return (
+//   const resp_product = await axios.get(`${URL}/products/${params.id}`);
+
+//   return {
+//       props: {
+//           product: resp_product.data,
+//       }
+//   }
+// };
+
+
+
+// //------COMPONENTE------//
+// export default function ProductPage({product}) {
+//   // const router = useRouter();
+//   // const {id} = router.query;
+
+//   return (
     
-    <div> 
-      <img src={product?.image} />
-      <h2>detalle del producto: {product?.id_product}</h2>
-      <h2>nombre: {product?.name} </h2>
-      <h2>precio: ${product?.price} </h2>
-    </div>
+//     <div> 
+//       <img src={product?.image} />
+//       <h2>detalle del producto: {product?.id_product}</h2>
+//       <h2>nombre: {product?.name} </h2>
+//       <h2>precio: ${product?.price} </h2>
+//     </div>
   
-  )
-}
+//   )
+// }
 
