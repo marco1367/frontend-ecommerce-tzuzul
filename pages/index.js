5437e36b0f7c66d4d64d66231a5f3cef95730c95
marco <marcopablodemaio@gmail.com>
2022-02-12 19:12:41 -0300

    WIP deploy

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import axios from 'axios';
-import dotenv from 'dotenv';
-
-
-export async function getServerSideProps() {
-
-  dotenv.config();
-  const { NEXT_PUBLIC_API } = process.env;
-
-  // axios.defaults.baseURL = NEXT_PUBLIC_API || 'http://localhost:8000';
-
-  return {
-    props: {}
-  }
-};
 
 export default function Home() {
   return (
