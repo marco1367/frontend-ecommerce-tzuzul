532f96cb7be41f71b80f3a8793372059b0c0cce9
marco <marcopablodemaio@gmail.com>
2022-02-13 02:45:16 -0300

    WIP deploy

---
+++ pages/products/[id].jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import axios from 'axios';
+
+
+
+//------getStaticPaths------//
+export async function getStaticPaths() {
+
+  const resp_products = await axios.get(`https://db-ecommerce-tzuzul.herokuapp.com/products`);
+
+  const paths = resp_products.data.map((product) =>
+   {
+      return {params:{
+        id: toString(product.id_product)
+      }}
+    }
+  );
+
+  return {
+    paths,
+    fallback: false
+  };
+}
+
+
+//------getStaticProps------//
+export async function getStaticProps({params}){
+
+  const resp_product = await axios.get(`https://db-ecommerce-tzuzul.herokuapp.com/products/${params.id}`);
+
+  return {
+      props: {
+          product: resp_product.data,
+      }
+  }
+};
+
+
+
+//------COMPONENTE------//
+export default function ProductPage({product}) {
+  // const router = useRouter();
+  // const {id} = router.query;
+
+  return (
+    
+    <div> 
+      <img src={product?.image} />
+      <h2>detalle del producto: {product?.id_product}</h2>
+      <h2>nombre: {product?.name} </h2>
+      <h2>precio: ${product?.price} </h2>
+    </div>
+  
+  )
+}
